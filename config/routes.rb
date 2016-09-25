3a5bf9b120438f02fc60a14be7affb2b2ceee13f
Michael Rode <Michaelrode44@gmail.com>
2016-09-25 00:35:33 -0400

    switched home page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   # root 'welcome#index'
 
-  root 'pages#home'
+  root 'game#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
