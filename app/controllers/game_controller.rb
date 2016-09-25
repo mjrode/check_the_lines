d286ea622d57301b06eddb6136a27219d8c731fc
Michael Rode <Michaelrode44@gmail.com>
2016-09-25 00:58:48 -0400

    changed point diff to 3

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,7 @@
 class GameController < ApplicationController
   def index
-    @line_games = Game.where('line_diff > ?', 5).order('line_diff DESC')
-    @over_under_games = Game.where('over_under_diff > ?', 5).order('over_under_diff DESC')
+    @line_games = Game.where('line_diff > ?', 3).order('line_diff DESC')
+    @over_under_games = Game.where('over_under_diff > ?', 3).order('over_under_diff DESC')
 
   end
 
