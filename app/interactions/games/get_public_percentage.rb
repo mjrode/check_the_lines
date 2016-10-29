9ef0c871921ba6c9828469acc0020186c0fcf836
Michael Rode <Michaelrode44@gmail.com>
2016-10-29 08:54:56 -0500

    test push

--- app/interactions/games/get_public_percentage.rb
+++ app/interactions/games/get_public_percentage.rb
@@ -26,7 +26,7 @@ class Games::GetPublicPercentage < Less::Interaction
     rows.each do |row|
       create_instance_variables(row) if valid_row(row)
       update_score if valid_row(row)
-      binding.pry if @away_team_name == "@ Toledo"
+      binding.pry if @home_team_name == "@ Utah St"
     end
   end
 
