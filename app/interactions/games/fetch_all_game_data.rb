021124c60c4b9b96f489ef76c887eee1cf9fa40e
Michael Rode <Michaelrode44@gmail.com>
2016-11-06 22:45:56 -0500

    Fixed last_month flag

--- app/interactions/games/fetch_all_game_data.rb
+++ app/interactions/games/fetch_all_game_data.rb
@@ -2,11 +2,11 @@ class Games::FetchAllGameData < Less::Interaction
 	expects :url
 	expects :date, allow_nil: true
 	expects :sport
-	expects :next_month, allow_nil: true
+	expects :last_month, allow_nil: true
 
 	def run
     Games::FetchMasseyData.run(url: url, sport: sport)
-    Games::FetchPublicPercentage.run(date: date, sport: sport, next_month: next_month)
+    Games::FetchPublicPercentage.run(date: date, sport: sport, last_month: last_month)
     Games::GameOver.run
     Games::FetchFinalScore.run(massey_url: url, sport: sport)
 		Games::CalculateAll.run()
