23d56a0a2855e2a68e7375d9b2f78a0df0518b45
Michael Rode <Michaelrode44@gmail.com>
2016-11-16 17:47:32 -0500

    Added ncaab to fetch today data interaction

--- app/interactions/games/fetch_todays_data.rb
+++ app/interactions/games/fetch_todays_data.rb
@@ -9,6 +9,7 @@ class Games::FetchTodaysData < Less::Interaction
 		Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=nba", sport: "nba")
 		Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=nfl", sport: "nfl")
 		Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=cf&sub=11604", sport: "ncaa_football")
+		Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=cb&sub=11590", sport: "ncaa_basketball")
   end
 
 end
