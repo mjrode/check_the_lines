346b58ee9bcd18f7f3188aadc3ed82b9320d55f1
Michael Rode <Michaelrode44@gmail.com>
2016-11-16 17:44:39 -0500

    Added ability to fetch old ncaabasketball games

---
+++ app/interactions/games/fetch_all_ncaa_basketball_games.rb
@@ -0,0 +1,21 @@
+class Games::FetchAllNcaaBasketballGames< Less::Interaction
+
+	def run
+		param_hash.each do |game|
+			Games::FetchAllGameData.run(url: game[:url], date: game[:date], sport: game[:sport], sportsbook_month: game[:sportsbook_month])
+		end
+	end
+
+	private
+	def param_hash
+		[
+			{ url: "http://www.masseyratings.com/nba/games?dt=20161111", date: "11", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/nba/games?dt=20161112", date: "12", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/nba/games?dt=20161113", date: "13", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/nba/games?dt=20161114", date: "14", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/nba/games?dt=20161115", date: "15", sport: "ncaa_basketball", sportsbook_month: "november"}
+		]
+	end
+end
+
+
