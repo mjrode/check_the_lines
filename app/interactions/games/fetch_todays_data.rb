42fd7e0889841c679c9d8c573a8f25beb9130cb2
Michael Rode <Michaelrode44@gmail.com>
2016-11-18 12:55:00 -0500

    test push

--- app/interactions/games/fetch_todays_data.rb
+++ app/interactions/games/fetch_todays_data.rb
@@ -7,15 +7,15 @@ class Games::FetchTodaysData < Less::Interaction
   private
 
   def load
-    Games::GameOver.run
-    Games::FetchFinalScore.run()
-		Games::CalculateAll.run()
-		unless flag
+ 		unless flag
 		  Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=nba", sport: "nba")
 		  Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=nfl", sport: "nfl")
 		  Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=cf&sub=11604", sport: "ncaa_football")
 		  Games::FetchAllGameData.run(url: "http://www.masseyratings.com/pred.php?s=cb&sub=11590", sport: "ncaa_basketball")
 		end
+		
+		Games::GameOver.run
+    Games::FetchFinalScore.run()
+		Games::CalculateAll.run()
   end
-
 end
