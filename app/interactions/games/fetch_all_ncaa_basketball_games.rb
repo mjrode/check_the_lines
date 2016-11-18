86d664218654e5c687d4e00306525dd75ee08a5b
Michael Rode <Michaelrode44@gmail.com>
2016-11-18 08:15:21 -0500

    Fixed fetch_all_ncaa_basketball_games

--- app/interactions/games/fetch_all_ncaa_basketball_games.rb
+++ app/interactions/games/fetch_all_ncaa_basketball_games.rb
@@ -9,11 +9,13 @@ class Games::FetchAllNcaaBasketballGames< Less::Interaction
 	private
 	def param_hash
 		[
-			{ url: "http://www.masseyratings.com/nba/games?dt=20161111", date: "11", sport: "ncaa_basketball", sportsbook_month: "november"},
-			{ url: "http://www.masseyratings.com/nba/games?dt=20161112", date: "12", sport: "ncaa_basketball", sportsbook_month: "november"},
-			{ url: "http://www.masseyratings.com/nba/games?dt=20161113", date: "13", sport: "ncaa_basketball", sportsbook_month: "november"},
-			{ url: "http://www.masseyratings.com/nba/games?dt=20161114", date: "14", sport: "ncaa_basketball", sportsbook_month: "november"},
-			{ url: "http://www.masseyratings.com/nba/games?dt=20161115", date: "15", sport: "ncaa_basketball", sportsbook_month: "november"}
+			{ url: "http://www.masseyratings.com/cb/11590/games?dt=20161111", date: "11", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/cb/11590/games?dt=20161112", date: "12", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/cb/11590/games?dt=20161113", date: "13", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/cb/11590/games?dt=20161114", date: "14", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/cb/11590/games?dt=20161115", date: "15", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/cb/11590/games?dt=20161116", date: "16", sport: "ncaa_basketball", sportsbook_month: "november"},
+			{ url: "http://www.masseyratings.com/cb/11590/games?dt=20161117", date: "17", sport: "ncaa_basketball", sportsbook_month: "november"}
 		]
 	end
 end
