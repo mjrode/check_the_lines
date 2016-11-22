c2c733bce549551879e6ce4deb5e8f8ddb9e42d7
Michael Rode <Michaelrode44@gmail.com>
2016-11-21 23:20:58 -0500

    Remove disqus

--- config/initializers/disqus_rails.rb
+++
@@ -1,6 +0,0 @@
-DisqusRails.setup do |config|
-  config::SHORT_NAME = "checkthelines."
-  config::SECRET_KEY = "LDFzVPsIqU1aVqPhpNfKImykf61ETQEyswZve4cqBtUo4kfiY6kFBfJyftbDZlIS" #leave blank if not used
-  config::PUBLIC_KEY = "Q31Cff2A1yy4IVLhu3DJ3zQDZy3s2MJoK74PU38UMwx3UcY8gi1YS3Yiby9ujzNA" #leave blank if not used
-  config::ACCESS_TOKEN = "6354310499dc4471b18c32e44f47913f" #you got it, right? ;-)
-end
