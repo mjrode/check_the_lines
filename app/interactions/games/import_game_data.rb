956c5b5c993edb9604feafba390e3c7cffb254ac
Michael Rode <michaelrode44@gmail.com>
2017-07-01 21:02:35 -0500

    Fix typos in import data class.

--- app/interactions/games/import_game_data.rb
+++ app/interactions/games/import_game_data.rb
@@ -9,7 +9,7 @@ class Games::ImportGameData < Less::Interaction
   def process_massey_and_wunder_data
     MasseyGame.unprocessed.each do |massey_game|
       wunder_game = WunderGame.where("sport = ? AND game_date = ?", massey_game.sport, massey_game.game_date)
-      .where('home_team_name ILIKE ? or away_team_name ILIKE ?', "%#{massey_game.home_team_name}%", "%#{massey_game.away_team_name}%")
+      .where('home_team_name ILIKE ? or away_team_name ILIKE ?', "%#{massey_game.home_team_name}%", "%#{massey_game.away_team_name}%").first
       game = find_or_create_game(massey_game, wunder_game)
       save_or_update_game(game, massey_game, wunder_game)
       mark_masssey_and_wunder_processed(wunder_game, massey_game)
@@ -20,7 +20,7 @@ class Games::ImportGameData < Less::Interaction
     Game.find_or_initialize_by(
       sport: massey_game.sport,
       date: massey_game.game_date,
-      external_id: wunder_game.external_id
+      home_team_name: wunder_game.home_team_name
     )
   end
 
@@ -40,7 +40,7 @@ class Games::ImportGameData < Less::Interaction
       away_team_massey_line: massey_game.away_team_massey_line,
       home_team_vegas_line: (wunder_game.home_team_vegas_line || massey_game.home_team_vegas_line),
       away_team_vegas_line:   (wunder_game.away_team_vegas_line || massey_game.away_team_vegas_line),
-      vegas_over_under: (wunder_game.vegas_over_under,
+      vegas_over_under: wunder_game.vegas_over_under,
       home_team_final_score: (wunder_game.home_team_final_score || massey_game.home_team_final_score),
       away_team_final_score: (wunder_game.away_team_final_score || massey_game.away_team_final_score),
       home_team_spread_percent: wunder_game.home_team_ats_percent,
