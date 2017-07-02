64c948b86ba372ab3cbec69ec2c9a59cecb4b5cd
Michael Rode <michaelrode44@gmail.com>
2017-07-02 10:41:57 -0500

    Allow FetchPastGameData to accept a range of dates.

--- app/interactions/games/fetch_past_game_data.rb
+++ app/interactions/games/fetch_past_game_data.rb
@@ -1,6 +1,8 @@
 class Games::FetchPastGameData < Less::Interaction
   expects :sport
-  expects :days_back
+  expects :days_back, allow_nil: true
+  expects :start_date, allow_nil: true
+  expects :end_date, allow_nil: true
 
   def run
     fetch_data
@@ -25,6 +27,7 @@ class Games::FetchPastGameData < Less::Interaction
   end
 
   def date_range
-    ((Date.today - days_back.days)..Date.today).map{ |date| date.strftime("%F").gsub("-","/") }
+    ((Date.today - days_back.days)..Date.today).map{ |date| date.strftime("%F").gsub("-","/") } if days_back.present?
+    (Date.parse(start_date)..Date.parse(end_date)).map{ |date| date.strftime("%F").gsub("-","/") }
   end
 end
