9814ebbf283a33d3a5686419d26de9a80d2ca233
Mike Rode <mike.rode@malauzai.com>
2019-10-13 00:45:00 -0500

    Remove old routes and rename SportController to GameController.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,39 +1,39 @@
 class GamesController < ApplicationController
-  def index
-  end
+  before_action :set_filter_params, :select_games_for_display
 
-  def show
-  end
-
-  def unplayed
-    @games = Game.all.unplayed.valid_spread
-  end
-
-  def played
-    @games = Game.all
-  end
-
-  def best_bets
-    @new_games = Game.all.unplayed.valid_spread.spread_best_bets
-    @over_under_games = Game.all.unplayed.valid_spread.over_under_best_bets
-  end
-
-  def old_best_bets
-    if params[:search].present?
-      @old_games = Game.all.played.valid_spread.spread_best_bets.where('strength > ?', params[:search].to_i)
-      @over_under_games = Game.all.played.valid_spread.over_under_best_bets.where('strength > ?', params[:search].to_i)
-    else
-      @old_games = Game.all.played.valid_spread.spread_best_bets.valid_spread
-      @over_under_games = Game.all.played.valid_spread.over_under_best_bets.valid_spread
-    end
-  end
-
-  def old_over_under_best_bets
-    @old_games = Game.all.played.valid_over_under.over_under_best_bets
+  def ncaaf
+    puts "Params -- #{params}"
   end
 
   def refresh
-    FetchGameData.new.fetch
-    redirect_to best_bets_games_path
+    fetch_and_update_game_data
+    redirect_to action: 'best_bets'
+  end
+
+  private
+
+  def fetch_and_update_game_data
+    puts "Fetching game data after controller action #{action_name}"
+    Jobs::ProcessCurrentData.run(process_all_games: true)
+    Jobs::ProcessAndUpdateGames.run(process_all_games: true)
+  end
+
+  def select_games_for_display
+    puts "Selecting games for display"
+    @games = Game.all
+    return @games = @games.not_over unless @sport_filter || @game_filter
+    @games = @games.send(@game_filter) if @game_filter
+    @games = @games.where(sport: format_sport_for_search(@sport_filter)) if @sport_filter && @sport_filter != 'all'
+    @games.order('date ASC')
+  end
+
+  def format_sport_for_search(sport)
+    sport_search_map = {'ncaaf': 'ncaaf'}
+    sport_search_map[sport.to_sym] || sport
+  end
+
+  def set_filter_params
+    @game_filter = params['game_filter']
+    @sport_filter = params['sport_filter']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,94 +1,15 @@
 Rails.application.routes.draw do
-
-  get 'pages/home'
-  get 'pages/about'
-
-  get 'pages/admin'
-
-  get 'pages/fetch_data'
-
   resources :games do
     collection do
-      get 'unplayed'
-      get 'played'
+      get 'ncaaf'
+      get 'refresh'
       get 'best_bets'
-      get 'old_best_bets'
-      get 'old_over_under_best_bets'
     end
   end
 
- resources :sports do
-   collection do
-     get 'ncaaf'
-     get 'refresh'
-   end
- end
-
- resources :admin do
-  collection do
-    post 'fetch_data'
+  resources :admin do
+    collection { post 'fetch_data' }
   end
- end
 
-  get 'ncaaf', to: 'sports#ncaaf'
-  get 'best_bets', to: 'sports#best_bets'
-  post 'ncaaf', to: 'sports#ncaaf'
-  get 'nfl', to: 'sports#nfl'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
-  # root 'welcome#index'
-  root 'sports#ncaaf'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  root 'games#home'
 end
